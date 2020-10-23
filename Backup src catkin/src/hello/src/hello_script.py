f6022a8e7dfe01a4836138ef3cbe7fcf5972a448
NeilSCGH <neil.segard@gmail.com>
2020-10-23 23:55:51 +0200

    deleting old files

--- Backup src catkin/src/hello/src/hello_script.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import rospy
-
-if __name__ == '__main__':
-	try:
-		rospy.init_node('hello_ros',anonymous=True)
-		rospy.loginfo("Hello, ROS!")
-	except rospy.ROSInterruptException:
-		pass
