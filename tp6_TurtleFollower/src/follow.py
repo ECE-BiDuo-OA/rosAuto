fcf5d10efb6dbaca85597c826f3f6282c0f6497c
NeilSCGH <neil.segard@gmail.com>
2020-10-26 11:08:54 +0100

    ca marche :)

--- tp6_TurtleFollower/src/follow.py
+++ tp6_TurtleFollower/src/follow.py
@@ -4,32 +4,42 @@ import numpy as np
 from geometry_msgs.msg import Twist, Point
 from turtlesim.msg import Pose
 
-xRef=2
+xRef=3
 yRef=2
 vRef=0
 
 Kp=1
 Uv=0
-Utheta=0
+Uw=0
 
 def callback(data):
-    global xRef,yRef,Uv, Utheta
+    global xRef, yRef, Uv, Uw
     x = data.x
     y = data.y
     theta = data.theta
     print("x: {}\ty: {}\ttheta: {}".format(x,y,theta))
     
-    phi=np.arctan((yRef - y)/(xRef-x))
+    phi=np.arctan2((yRef - y),(xRef-x))
     diff = theta - phi
-    w=-Kp * diff
     
-    Utheta = theta+ 0.1 * w
-    Uv=vRef
-    print("Utheta: {}\tUv: {}".format(Utheta,Uv))
+    if diff >= np.pi:	diff -= 2*np.pi
+    if diff < -np.pi:	diff += 2*np.pi
+    print("diff: {}".format(diff))
+    
+    Uw=-Kp * diff
+    if diff <= 0.01: Uw = 0.0
+    
+    dist = np.sqrt((yRef-y)**2+(xRef-x)**2)
+    Uv=min(dist,1)
+    if dist <= 0.01: Uv = 0.0
+            
+    print("Uw: {}\tUv: {}".format(Uw,Uv))
+    
+    print("Distance to target: {}".format(dist))
     
 
 def talker():
-    global Uv, Utheta
+    global Uv, Uw
     rospy.init_node('talker', anonymous=True)
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     rospy.Subscriber('/turtle1/pose', Pose, callback)
@@ -49,7 +59,7 @@ def talker():
     while not rospy.is_shutdown():
         speed = Twist()
         speed.linear.x = Uv
-        speed.angular.z = Utheta
+        speed.angular.z = Uw
         #print(speed)
         pub.publish(speed)
         rate.sleep()
