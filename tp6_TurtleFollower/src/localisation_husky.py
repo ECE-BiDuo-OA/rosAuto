15243cd2a768570a090a104066e1243bfbad4bde
NeilSCGH <neil.segard@gmail.com>
2020-11-22 13:34:58 +0100

    gps estimation done

--- tp6_TurtleFollower/src/localisation_husky.py
+++ tp6_TurtleFollower/src/localisation_husky.py
@@ -6,28 +6,48 @@ from geometry_msgs.msg import Vector3Stamped
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Imu
 
+global xGPS, yGPS, vxGPS, vyGPS, oldvxGPS, oldvyGPS
+global realx, realy
+xGPS = yGPS = vxGPS = vyGPS = realx = realy =0
+
 def callbackImu(data):
-    theta=data.pose.pose.orientation.w
-    print("theta={:.3f}".format(tetha))
+    theta=data.orientation.w
+    time=data.header.stamp
+    #print("theta={:.3f} time={}".format(theta,time))
 
 def callbackGPS(data):
-    x=data.Vector3.x
-    y=data.Vector3.y
-    print("x={:.3f} y={:.3f}".format(x,y))
+    global xGPS, yGPS, vxGPS, vyGPS, oldvxGPS, oldvyGPS
+    dt=0.020 #seconds
+    
+    oldvxGPS=vxGPS
+    oldvyGPS=vyGPS
+    
+    vxGPS=data.vector.x
+    vyGPS=data.vector.y
+    
+    xGPS += (oldvxGPS+vxGPS) * dt/2
+    yGPS += (oldvyGPS+vyGPS) * dt/2
+    
+    
+    
 
 def callbackHuskyVel(data):
     vx=data.twist.twist.linear.x
     vy=data.twist.twist.linear.y
     wx=data.twist.twist.angular.x
     wy=data.twist.twist.angular.y
-    print("v={:.3f} w={:.3f}".format(v,w))
+    #print("vx={:.3f} vy={:.3f}".format(vx,vy))
+    #print("wx={:.3f} wy={:.3f}".format(wx,wy))
     
 def solution(data):
-    x=data.pose.pose.position.x
-    y=data.pose.pose.position.y
-    print("x={:.3f} y={:.3f}".format(x,y))
+    global realx, realy
+    realx=data.pose.pose.position.x
+    realy=data.pose.pose.position.y
+    
 
 def localisateur():
+    global xGPS, yGPS, realx, realy
+
     rospy.init_node('localisateur', anonymous=True)
     #pub = rospy.Publisher('/destination', Point, queue_size=10)
     rospy.Subscriber('/imu/data', Imu, callbackImu)
@@ -38,6 +58,9 @@ def localisateur():
     #rate = rospy.Rate(10) # 1hz
     
     while not rospy.is_shutdown():
+        print("")
+        print("xGPS={:.3f} yGPS={:.3f}".format(xGPS,yGPS))
+        print("realx={:.3f} realy={:.3f}".format(realx,realy))
         
         time.sleep(1)
         #rate.sleep()
